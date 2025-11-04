d7a4f1961197ae958bae55822fcbf46181be8e2a
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-04 23:49:02 +0530

    3 digit question

--- 11_Arrays/Question5.java
+++ 11_Arrays/Question5.java
@@ -1,11 +1,13 @@
-public class Question1 {
+// Question: What is the output of this program that finds three-digit numbers in an array?
+
+public class Question5 {
     public static void main(String[] args) {
         int[] nums = {10, 21, 31, 415, 50, 60, 700};
-        accessEvenIndex(nums);
+        findThreeDigitNumbers(nums);
     }
-    public static void accessEvenIndex(int[] arr){
+    public static void findThreeDigitNumbers(int[] arr){
         for(int i=0; i<arr.length; i++){
-            if(arr[i]>= 100 && nums[i] <= 999){
+            if(arr[i]>= 100 && arr[i] <= 999){  // Fixed: changed nums[i] to arr[i]
                 System.out.println(arr[i] + " ");
             }
         }
