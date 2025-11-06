dcf32823d5981c15a543faf7ddbffc26d922d6f1
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-06 23:06:50 +0530

    Java Array

--- 11_Arrays/Project1/Employ.java
+++ 11_Arrays/Project1/Employ.java
@@ -1,18 +1,17 @@
-public class Employ {
-    String name;
-    int id;
-    int age;
-    double salary;
-    Employ(){
-
-    }
-     Employ(String name, int id, int age, double salary){
-        this.name = name;
-        this.id = id;
-        this.age = age;
-        this.salary = salary;
-    }
-    public String toString(){
-        return "Name: " + name+ "\tId: " + id+"\tAge: " + age+"\tSalary: " + salary;
-    }
+class Employ{
+	String name;
+	int id;
+	int age;
+	double salary;
+	Employ(){
+	}
+	Employ(String name, int id, int age, double salary){
+		this.name=name;
+		this.id=id;
+		this.age=age;
+		this.salary=salary;
+	}
+	public String toString(){
+	return "Name: "+name+"\tId: "+id+"\tAge: "+age+"\tSalary: "+salary; 
+	}
 }
---
+++ 11_Arrays/Swap1.java
@@ -0,0 +1,24 @@
+public class Swap1 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        int[] b = {60, 70, 80, 90, 100};
+        System.out.println("before swap: ");
+
+        displayValues(a);
+        displayValues(b);
+
+
+        int[] temp = a;
+        a=b;
+        b=temp;
+
+        System.out.println("After Swap: ");
+        displayValues(a);
+        displayValues(b);
+    }
+    public static void displayValues(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
