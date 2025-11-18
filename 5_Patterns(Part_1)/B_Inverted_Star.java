c2b41797147461a8a98b88693a3e5ac931d474e6
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-18 22:37:39 +0530

    Pattern Questions

---
+++ 5_Patterns(Part_1)/B_Inverted_Star.java
@@ -0,0 +1,17 @@
+class B_Inverted_Star {
+	public static void main(String[] args) {
+		int n = 4;
+		for(int line = 1; line <=4; line++){
+			for(int star = 1; star <= (n - line + 1); star++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		// for (int line = 1; line <= 4; line++) {
+		// 	for (int star = 4; star >= line; star--) {
+		// 		System.out.print("*");
+		// 	}
+		// 	System.out.println();
+		// }
+	}
+}
---
+++ 5_Patterns(Part_1)/C_Half_Pyramid.java
@@ -0,0 +1,5 @@
+public class C_Half_Pyramid {
+    public static void main(String[] args) {
+        System.out.println("vfv");
+    }
+}
