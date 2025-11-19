49756f3175d5aae7c775fe62c22c467b45780848
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-19 11:39:03 +0530

    Half Pyramid && Char Pattern

--- 5_Patterns(Part_1)/C_Half_Pyramid.java
+++ 5_Patterns(Part_1)/C_Half_Pyramid.java
@@ -1,5 +1,11 @@
 public class C_Half_Pyramid {
     public static void main(String[] args) {
-        System.out.println("vfv");
+        int n = 5;
+        for (int line = 1; line <= n; line++) {
+            for (int num = 1; num <= line; num++) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
     }
 }
