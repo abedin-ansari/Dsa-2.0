c0557001979feb5f64524a87efdf879f2abf76aa
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-22 23:10:36 +0530

    Prime number by Function

---
+++ 6_FunctionsOrMethods/C_PrimeNum.java
@@ -0,0 +1,17 @@
+public class C_PrimeNum {
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(isPrime(n));
+    }
+}
