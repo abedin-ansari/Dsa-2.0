844f5b155ad0a9206732848b126671df9f1ebed0
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-26 22:35:52 +0530

    Array Inp Opt

---
+++ 8_Arrays/InputAndOutput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputAndOutput {
+    public static void main(String[] args) {
+        int[] marks = new int[50];
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter marks of Physics, Chemistry and Maths: ");
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        int arrLength  = marks.length;
+        System.out.println("Length of array: " + arrLength);
+
+        System.out.println("Physics: " + marks[0]);
+        System.out.println("Chemestry: " + marks[1]);
+        System.out.println("Maths: " + marks[2]);
+
+        // marks[2] = 71;
+        // System.out.println("Updated Maths marks: " + marks[2]);
+
+        int percentage = ((marks[0] + marks[1] + marks[2]) / 3);
+        System.out.println("Percentage: " + percentage);
+    }
+}
