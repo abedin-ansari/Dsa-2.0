c681ada715c43c9758989f040219c01dcd728460
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-26 23:22:38 +0530

    Update Array Val && Array InputOutput

--- 8_Arrays/A_InputAndOutput.java
+++ 8_Arrays/A_InputAndOutput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InputAndOutput {
+public class A_InputAndOutput {
     public static void main(String[] args) {
         int[] marks = new int[50];
 
---
+++ 8_Arrays/B_UpdateArrayVal.java
@@ -0,0 +1,16 @@
+public class B_UpdateArrayVal {
+    public static void main(String[] args) {
+        int[] marks = {80, 75, 66};
+
+        update(marks);
+
+        for(int i=0; i< marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    public static void update(int[] marks){
+        for(int i =0; i< marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+}
