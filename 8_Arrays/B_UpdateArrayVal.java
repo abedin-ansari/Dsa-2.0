06f34ec893c456fbab18d25667e673fc3381db74
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2025-11-29 22:51:49 +0530

    Updated code

--- 8_Arrays/B_UpdateArrayVal.java
+++ 8_Arrays/B_UpdateArrayVal.java
@@ -2,8 +2,9 @@ public class B_UpdateArrayVal {
     public static void main(String[] args) {
         int[] marks = {80, 75, 66};
 
-        update(marks);
+        update(marks); // 
 
+        // print updated marks
         for(int i=0; i< marks.length; i++){
             System.out.println(marks[i]);
         }
